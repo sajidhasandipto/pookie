3e97dd8a3a7a2a86b41511b4b63cbaf5f8560db5
Sajid Hasan Dipto <sajidhasandipto.work@gmail.com>
2026-03-04 21:18:31 +0600

    feat(Shorts): added layout for shortsPage

---
+++ src/layout/shorts/ShortsDiv.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShortsDiv = () => {
+  return (
+    <div className='text-white'>ShortsDiv</div>
+  )
+}
+
+export default ShortsDiv
--- src/pages/ShortsPage.jsx
+++ src/pages/ShortsPage.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import Sidebar from '../layout/sidebar/Sidebar'
 import { SidebarProvider } from '../layout/sidebar/SidebarContext'
+import PageHeader from '../layout/PageHeader'
+import ShortsDiv from '../layout/shorts/ShortsDiv'
 
 const ShortsPage = () => {
   return (
-    <div>
-      <SidebarProvider>
-        <Sidebar defaultActiveChoice='Shorts' />
-      </SidebarProvider>
-    </div>
+    <SidebarProvider>
+      <div className='max-h-screen flex flex-col bg-zinc-950'>
+        <PageHeader />
+        <div className='grid grid-cols-[auto_1fr] grow overflow-auto'>
+          <div className='overflow-y-auto grow scrollbar-yt overflow-x-hidden text-white'>
+            <Sidebar defaultActiveChoice='Shorts' />
+          </div>
+          <div className='overflow-x-hidden scrollbar-yt px-8 pb-4'>
+            {/* shorts div */}
+            <ShortsDiv/>
+          </div>
+
+        </div>
+
+      </div>
+    </SidebarProvider>
   )
 }
 
